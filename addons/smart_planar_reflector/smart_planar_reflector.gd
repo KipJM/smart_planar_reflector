3059dd80201034dddeb83068807fa1f2047f352f
Kip <jeretech.jeremy@gmail.com>
2025-07-23 17:10:18 +0800

    plugin kinda done! Added comments and a demo scene

--- addons/smart_planar_reflector/smart_planar_reflector.gd
+++ addons/smart_planar_reflector/smart_planar_reflector.gd
@@ -4,9 +4,9 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
+	add_custom_type("PlanarReflector", "MeshInstance3D", preload("Internal/PlanarReflector.gd"), preload("Internal/PlanarReflector.svg"))
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	remove_custom_type("PlanarReflector")
